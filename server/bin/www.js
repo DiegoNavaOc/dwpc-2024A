36caf2382e42519b136be9dccdd312acc1398807
DiegoNavaOc <158502661+DiegoNavaOc@users.noreply.github.com>
2024-02-17 03:44:41 +0000

    🎇Feat: Refactoring to ES6 Modernizando el código JavaScript creado por el generador de express.

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,9 +4,10 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('dwpc-2024a:server');
-var http = require('http');
+import app from '../app';
+import debugLib from 'debug';
+import http from 'http';
+const debug = debugLib('expweb-bp:server'); 
 
 /**
  * Get port from environment and store in Express.
@@ -33,21 +34,18 @@ server.on('listening', onListening);
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
+function normalizePort(val) { 
+    const port = parseInt(val, 10); 
+    if (Number.isNaN(port)) { 
+      // named pipe 
+      return val; 
+    } 
+    if (port >= 0) { 
+      // port number 
+      return port; 
+    } 
+    return false; 
+  } 
 
 /**
  * Event listener for HTTP server "error" event.
@@ -58,7 +56,7 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
+  const bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
 
@@ -82,8 +80,8 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
+  const addr = server.address();
+  const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
