00e58d363009e8c5b74528fef3699da66e7e73f6
DiegoNavaOc <158502661+DiegoNavaOc@users.noreply.github.com>
2024-03-13 01:52:04 +0000

    ⭐Feature: AirBnB Style Guide

--- client/index.js
+++ client/index.js
@@ -1,31 +1,4 @@
-//Cargando estilos
-import './styles/style.css'
+// Cargando estilos
+import './styles/style.css';
 
-console.log("Webpack Working!!! 📦");
-
-// Codigo ES6
-// Default Parameters
-let show = (msg="No message given") => {
-  console.log(msg)
-}
-show();
-
-// Async Await
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000)
-  });
-}
-
-// Function asincrona
-async function asyncCall() {
-  console.log("Calling");
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-}
-
-// Llamar a la función asincrona
-asyncCall();
-show();
+console.log('Webpack Working!!! 📦');
